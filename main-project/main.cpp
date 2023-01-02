162d0b90e15254d741b8ee0b3dab9b9e81552c25
PozziTiv4ikRB <pozzitiv4ik2281337@mail.ru>
2023-01-02 16:27:48 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #9. GIT\n";
     cout << "Variant #4. Exchange rates\n";
     cout << "Author: Michael Duakonov\n";
+    cout << "Group: 12\n";
     return 0;
 }
